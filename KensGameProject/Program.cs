4ed120e2ae0b42262a82a0da8bc2929b79c42fb0
Greg Doud <gpdoud@gmail.com>
2016-12-19 14:22:49 -0500

    Added Randomness class

--- KensGameProject/Program.cs
+++ KensGameProject/Program.cs
@@ -4,10 +4,24 @@ namespace KensGameProject {
     
     class MainClass {
 
+        void TestRandomness() {
+            var t = 0;
+            var f = 0;
+            for (var i = 0; i < 100000; i++) {
+                var tf = Randomness.TrueOrFalse(25);
+                if (tf) {
+                    t++;
+                } else {
+                    f++;
+                }
+            }
+            Console.WriteLine("True: {0}; False: {1}", t, f);
+        }
+
         void Run() {
-            Character knight = new Character("Balthazar", Class.Pyromancer);
-            Character sorceror = new Character("Leprechaun", Class.Cleric);
-            Character winner = Character.Battle(knight, sorceror);
+            var knight = new Character("Balthazar", Class.Pyromancer);
+            var sorceror = new Character("Leprechaun", Class.Cleric);
+            var winner = Character.Battle(knight, sorceror);
             Console.WriteLine("Winner is {0}", winner.Name);
         }
         public static void Main(string[] args) {
---
+++ KensGameProject/Randomness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace KensGameProject {
+
+    public class Randomness {
+
+        static Random rnd = null;
+
+        public static bool TrueOrFalse(int truePercent = 50) {
+            int nbrNeeded = 100 / truePercent;
+            return rnd.Next(nbrNeeded) == 0;
+        }
+    
+        public Randomness() {
+        }
+
+        static Randomness() {
+            rnd = new Random(DateTime.UtcNow.GetHashCode());
+        }
+    }
+}
